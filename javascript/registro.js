9ca06d8f36d8887d5c4f685f75c0343dd1e792c5
Lucas Otero <luotero.99@gmail.com>
2022-06-17 21:14:32 -0300

    Agrego eventos e interactuo con el DOM mediante el formulario de registro

--- javascript/registro.js
+++ javascript/registro.js
@@ -1,4 +1,5 @@
 // Registrar usuario y tras darle al botón se pushea al array
+// También aparece el usuario en el HTML con el .value, (Desafío DOM)
 
 const submitButton = document.getElementById("submit-button")
 submitButton.addEventListener("click", registrarUsuario)
@@ -7,11 +8,11 @@ submitButton.addEventListener("click", registrarUsuario)
 const usuariosRegistrados = []
 
 function capturarUsuario() {
-    let nombre = document.getElementById("nombre").value 
-    let apellido = document.getElementById("apellido").value 
-    let email = document.getElementById("email").value 
-    let password = document.getElementById("password").value 
-    let repassword = document.getElementById("repassword").value
+    let nombre = document.getElementById("nombre").value = "Samuel"
+    let apellido = document.getElementById("apellido").value = "Tocamaiza"
+    let email = document.getElementById("email").value = "samumaiza@gmai.com"
+    let password = document.getElementById("password").value = "cursoJS"
+    let repassword = document.getElementById("repassword").value = "cursoJS"
 
         console.log(`nombre: ${nombre} \n apellido: ${apellido} \n email: ${email} \n contraseña: ${password} \n contraseña confirmada: ${repassword}`)
 }
